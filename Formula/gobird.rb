edcd1c6695813716f150ed4453754bb37ec881ba
mudrii <mudreac@gmail.com>
2026-03-24 17:32:35 +0800

    Update gobird formula checksums for 26.03.24

--- Formula/gobird.rb
+++ Formula/gobird.rb
@@ -10,22 +10,22 @@ class Gobird < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/mudrii/gobird/releases/download/26.03.24/gobird_26.03.24_darwin_amd64.tar.gz"
-      sha256 "a5b95d41207a6f5d872eca36c6e68f16913d43006ac989d46ff4f7320e19d88f"
+      sha256 "7dd8574bf46922e6a65139951bdd097c56e6767f76590ab0a0bfd8db2c5b6008"
     end
     if Hardware::CPU.arm?
       url "https://github.com/mudrii/gobird/releases/download/26.03.24/gobird_26.03.24_darwin_arm64.tar.gz"
-      sha256 "365367f3174b8534156747618710706e29d2341e6352469b5470068565356852"
+      sha256 "fb954f22bce7f13d95f16fc8a4583164253f09dcb27600e05db38ad797a82154"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/mudrii/gobird/releases/download/26.03.24/gobird_26.03.24_linux_amd64.tar.gz"
-      sha256 "e758591ccda0b0524926c2416cbaf77591ee334a5ecf3cfb042dd84605e7aaeb"
+      sha256 "2be269823f53d047cba67b5e4a285284d8d42bfedadbb5bdf3e813a0e9f86ffe"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/mudrii/gobird/releases/download/26.03.24/gobird_26.03.24_linux_arm64.tar.gz"
-      sha256 "f4c387d2c455e59afc7ec704ac76ccdd2b80e34c4dafd1b31191d742f035ceb4"
+      sha256 "901215e2643975ee1735e07842ba2f78463b402a01a4176255de7125b542bb65"
     end
   end
 
